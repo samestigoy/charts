44fa97038cdc2afbb713006a68fcbf1c6ff79aa9
Sam Estigoy <samuelestigoy@gmail.com>
2022-02-07 20:16:59 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 
 
-/* //GET request for data
-function getPhotos() {
-    axios
-        .get('https://jsonplaceholder.typicode.com/photos')
-        //.then(res => showOutput(res))
-        .then(res => getAlbumIDCount(res))
-        .catch(err => console.error(err))
-} */
 
 //GET Bitcoin Data
 function getBitCoinData() {
@@ -66,74 +58,6 @@ function displayBitcoinData(res) {
         </div>
     `;
 }
-
-/* 
-//Save albumID and count number of IDs in each Album
-function getAlbumIDCount(res) {
-    let albumIDList = [];
-    let resData = res.data;
-    for (let i = 0; i < resData.length; i++) {
-        albumIDList.push(resData[i]["albumId"]);
-        
-    }
-    let uniqueAlbumIDList = [... new Set(albumIDList)];
-
-    console.log(resData[0]);
-    //console.log(res.data);
-    console.log(uniqueAlbumIDList);
-
-    document.getElementById('dataTable').innerHTML = `
-        <div class="card mt-3">
-            <div class="card-header">
-                Data
-            </div>
-            <div class="card-body">
-                <pre>${uniqueAlbumIDList}</pre>
-            </div>
-        </div>
-    `;
-}
- */
-
-
-/* function showOutput(res) {
-    let albumIDList = getAlbumIDCount(res);
-
-    document.getElementById('res').innerHTML = `
-    <div class="card card-body mb-4">
-      <h5>Status: ${res.status}</h5>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Headers
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-      </div>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Data
-      </div>
-      <div class="card-body">
-        <pre>${albumIDList}</pre>
-        <pre>${JSON.stringify(res.data, null, 2)}</pre>
-        
-      </div>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Config
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.config, null, 2)}</pre>
-      </div>
-    </div>
-  `;
-  } */
 
 //Event listeners
 document.getElementById('get').addEventListener('click', getBitCoinData);
